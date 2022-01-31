d0dffa2b672d4e588aa4c1c3aef3346628450292
arunganeshan <arunganesh46@yahoo.in>
2022-01-31 19:14:23 +0530

    user dictionay

---
+++ src/org/test/Globalpages.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Globalpages {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println("output is "+c);		
+	}
+
+
+}
+
